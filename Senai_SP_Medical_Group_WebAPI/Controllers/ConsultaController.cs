861506037de3a27cfc5552fa78d35e50d2208cb9
Tamashi19 <29raul19@gmail.com>
2021-11-12 17:24:46 -0300

    .

---
+++ Senai_SP_Medical_Group_WebAPI/Controllers/ConsultaController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai_SP_Medical_Group_WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConsultaController : ControllerBase
+    {
+    }
+}
---
+++ Senai_SP_Medical_Group_WebAPI/Controllers/PacienteController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai_SP_Medical_Group_WebAPI.Controllers
+{
+    public class PacienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
